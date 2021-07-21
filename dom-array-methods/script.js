d59607a5aa88d8e9f9bda637e04f9d7e8dc5a3e8
Igor Tarchugovski <Igor.t93@outlook.com>
2021-07-21 17:49:27 +0200

    sortByRichest-sort()

--- dom-array-methods/script.js
+++ dom-array-methods/script.js
@@ -35,6 +35,13 @@ function doubleMoney() {
   updateDOM();
 }
 
+// Sort users by richest
+function sortByRichest() {
+    data.sort((a, b) => b.money - a.money);
+
+    updateDOM();
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
@@ -63,5 +70,6 @@ function formatMoney(number) {
 }
 
 // Event listeners
-addUserBtn.addEventListener("click", getRandomUser);
-doubleBtn.addEventListener("click", doubleMoney);
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
